4a5a7c0b44c6b199c64cee3ae57d0b0dae1ad1fa
luissejasmendez <luis@miraxr.io>
2024-04-11 21:37:44 -0400

    Fixed the spaces between letter

--- components/home/main.tsx
+++ components/home/main.tsx
@@ -10,19 +10,19 @@ const Main = () => {
           alt=""
           className="absolute h-full w-full"
         />
-        <div className="relative w-[70%]">
-          <div className="flex flex-col mt-52 gap-2">
-            <div className="text-neutral-50 text-left text-6xl md:text-[128px] font-bold font-['Iowan Old Style'] md:leading-[70px]">
+        <div className="relative p-8 mb-36">
+          <div className="flex flex-col mt-52  md:gap-2">
+            <div className="text-neutral-50 text-left text-[60px] leading-3 md:text-[128px] font-bold font-['Iowan Old Style'] md:leading-[70px]">
               Immersive
             </div>
-            <div className="text-left text-neutral-50 text-7xl md:text-[175px] font-bold font-['Iowan Old Style'] md:leading-[150px]">
+            <div className="text-left text-neutral-50  text-[72px] leading-2 md:text-[175px] font-bold font-['Iowan Old Style'] md:leading-[150px]">
               Media
             </div>
-            <div className="text-left text-neutral-50 text-6xl md:text-[128px] font-bold font-['Iowan Old Style'] md:leading-[100px]">
+            <div className="text-left text-neutral-50 text-[60px] leading-3 md:text-[128px] font-bold font-['Iowan Old Style'] md:leading-[80px]">
               Studio
             </div>
           </div>
-          <div className="text-white text-xl font-medium font-['Inter'] leading-tight mt-24 border-t border-white p-2 mb-40">
+          <div className="text-white text-sm md:text-xl font-medium font-['Inter'] leading-tight mt-12 md:mt-24 border-t border-white p-2 ">
             Virtual Reality | Augmented Reality | Mixed Reality
           </div>
         </div>
